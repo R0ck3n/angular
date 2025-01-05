e01e7e365ec2f719a3ea663594e6c6fe1948d3c2
Rocken <r0ck3n@hotmail.com>
2025-01-05 18:00:15 +0100

    1er composant creer

---
+++ projects/angular/src/app/features/games/components/list-games/list-games.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-games',
+  imports: [],
+  templateUrl: './list-games.component.html',
+  styleUrl: './list-games.component.css'
+})
+export class ListGamesComponent {
+
+}
